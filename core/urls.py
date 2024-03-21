8f2085f03f310fa2fe9067bd8829ca3f65d15fe2
Leigh Michael Forrest <leigh@catbirdseat.io>
2024-03-20 20:05:08 -0400

    basic media added

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -7,3 +9,6 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('books/', include('books.urls')),
 ]
+
+if not settings.S3: 
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
